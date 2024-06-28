defb100e1837b626d1d6e4d67bf95d7ad91666f9
sweetpotato15 <susan6439@naver.com>
2024-06-28 21:30:10 +0900

    hanyang_info

--- hanyang_info.py
+++ hanyang_info.py
@@ -1,45 +1,46 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import pandas as pd
-import time
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-# WebDriver 옵션 설정 (Headless 모드로 설정)
-options = Options()
-options.headless = True
+# years = [2019,2020,2021,2022,2023,2024]
+# months = list(range(1,13))
+
+years = [2024]
+months = list(range(4,6))
+
+months = [str(x).zfill(2) for x in months]
+base_url = 'https://www.hanyang-u.hs.kr/?c=C01/C12/C121&sort=d_regis&orderby=desc&uid=&nyear={}&nmonth={}&nday=28'
+
+for year in years:
+    for month in months:
+        menu_info = []
+        # 웹페이지 URL
+        url = base_url.format(year, month)
+
+        # HTTP GET 요청
+        response = requests.get(url)
+
+        # 요청이 성공했는지 확인
+        if response.status_code == 200:
+            # BeautifulSoup 객체 생성
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # 텍스트 가져오기
+            text_elements = soup.select('tbody > tr > td')
+            for text in text_elements:
+                menu_info.append(text.text)
+
+            csv_file = f'{year}_{month}.csv'
+            with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+
+                for menu in menu_info:
+                    writer.writerow([menu])
+                # writer.writerow(['날짜', '구분', '이미지 URL'])  # 헤더를 작성합니다.
+                # writer.writerows([menu_info])  # 데이터를 CSV 파일에 작성합니다.
+
+            print(f'CSV 파일 {csv_file}이(가) 성공적으로 생성되었습니다.')
 
 
-# WebDriver 초기화
-driver = webdriver.Chrome(options=options)
-
-# 웹 페이지 열기
-url = 'https://www.hanyang-u.hs.kr/?c=C01/C12/C121&sort=d_regis&orderby=desc&uid=&nyear=2024&nmonth=06&nday=28'
-driver.get(url)
-
-# tbody > tr > text
-# 정보 추출을 위한 CSS selector 정의
-menu_boxes = driver.find_elements(By.CSS_SELECTOR, '.sbjx')
-
-# 데이터 저장을 위한 리스트 초기화
-data = []
-
-# 정보 추출
-for menu_box in menu_boxes:
-    category = menu_box.find_element(By.CSS_SELECTOR, '.cat').text.strip()
-    date_menu = menu_box.find_element(By.CSS_SELECTOR, 'a').text.strip()
-    data.append([category, date_menu])
-
-# WebDriver 종료
-driver.quit()
-
-# 추출한 데이터를 DataFrame으로 변환
-df = pd.DataFrame(data, columns=['Category', 'Date_Menu'])
-print(df)
-
-
-# # CSV 파일로 저장
-# csv_file_path = 'hanyang_menu.csv'
-# df.to_csv(csv_file_path, index=False)
-
-# print(f"CSV 파일이 저장되었습니다: {csv_file_path}")
+        else:
+            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
